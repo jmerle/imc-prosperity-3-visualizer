259751b1da6bd2974bd4ac06d5f1d15ab9072c49
Jasper van Merle <jaspervmerle@gmail.com>
2025-04-17 00:23:29 +0200

    Fix positions chart line colors

--- src/pages/visualizer/PositionChart.tsx
+++ src/pages/visualizer/PositionChart.tsx
@@ -68,7 +68,7 @@ export function PositionChart({ symbols }: PositionChartProps): ReactNode {
 
     // We offset the position color by 1 to make it line up with the colors in the profit / loss chart,
     // while keeping the "Total" line in the profit / loss chart the same color at all times
-    colorIndex: i + 1,
+    colorIndex: (i + 1) % 10,
   }));
 
   return <Chart title="Positions (% of limit)" series={series} min={-100} max={100} />;
